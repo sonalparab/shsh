8531b0ae6215dc547ec2480d9ff9abc671d45626
Helen Ye <00yhelen@gmail.com>
2017-11-22 11:25:10 -0500

    Fix errors and make shell-like

--- main.c
+++ main.c
@@ -6,19 +6,32 @@
 #include <sys/types.h>
 
 int main() {
-    char buffer[16];
+    char buffer[256];
     char *cmd = buffer;
-    char *args[16] = {};
+    char *arg;
+    char *args[256];
 
     printf(">>> ");
     while(fgets(buffer, sizeof(buffer), stdin)) {
-        cmd[sizeof(cmd)-1] = 0;
+        cmd = buffer;
+        buffer[strlen(buffer) - 1] = 0;
+
         int i;
-        // SEGFAULTS
         for (i = 0; cmd; i++) {
-            args[i] = strsep(&cmd, " ");
-            printf("args %d: %s\n", i, args[i]);
+            arg = strsep(&cmd, " ");
+            args[i] = arg;
         }
+
+        args[i] = 0;
+
+        int f = fork();
+        if (f == 0) {
+            execvp(args[0], args);
+        }
+
+        int status;
+        wait(&status);
+
         printf(">>> ");
     }
 }
